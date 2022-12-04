9766a3ceca3690431d35af403531ebcd1a676361
Lily Russ <llyruss@yahoo.com>
2022-12-03 20:53:57 -0600

    first attempt at useRoutes

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,75 @@
+const router = require('express').Router();
+const { User, Post } = require('../../models');
+
+
+
+//create account
+router.post('/', async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+
+    req.session.save(() => {
+      req.session.userId = userData.dataValues.id;
+      req.session.userName = userData.dataValues.userName;
+      req.session.logged_in = true;
+
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
+});
+
+
+//login
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { userName: req.body.userName } });
+    
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect username or password, please try again' });
+      return;
+    }
+
+    const validPassword = userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect username or password, please try again' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.userId = userData.dataValues.id;
+      req.session.userName = userData.dataValues.userName;
+      req.session.logged_in = true;
+      
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
+});
+
+//logout
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+  res.json({message: "You are now logged out"})
+});
+
+
+
+module.exports = router;
